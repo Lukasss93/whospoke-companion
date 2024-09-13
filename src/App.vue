08e7e20ee345214b23199995c18d54906609adc5
Luca Patera <lucapatera@outlook.it>
2024-09-13 21:52:00 +0200

    Add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <div>
-    <a href="https://electron-vite.github.io" target="_blank">
-      <img src="/electron-vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://whospoke.lucapatera.it/@Sikuro" target="_blank">
+      <img src="/logo.svg" style="width: 80px" alt="Logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld>
+      <p class="text-amber-100 font-bold">Who Spoke</p>
+      <p class="text-gray-500 font-bold text-sm">Companion</p>
+  </HelloWorld>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
