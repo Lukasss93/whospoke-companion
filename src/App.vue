aca79f8e70bcccf357b265cba8b6f1e17a919c83
Luca Patera <lucapatera@outlook.it>
2024-09-14 17:53:14 +0200

    Remove dummy component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-
 </script>
 
 <template>
-  <div>
-    <a href="https://whospoke.lucapatera.it/@Sikuro" target="_blank">
-      <img src="/logo.svg" style="width: 80px" alt="Logo" />
-    </a>
-  </div>
-  <HelloWorld>
-      <p class="text-amber-100 font-bold">Who Spoke</p>
-      <p class="text-gray-500 font-bold text-sm">Companion</p>
-  </HelloWorld>
+    <div>
+        <a href="https://whospoke.lucapatera.it/@Sikuro" target="_blank">
+            <img src="/logo.svg" style="width: 80px" alt="Logo"/>
+        </a>
+    </div>
+    <p class="text-amber-100 font-bold">Who Spoke</p>
+    <p class="text-gray-500 font-bold text-sm">Companion</p>
+
+    <p>WIP</p>
 </template>
 
-<style scoped>
+<style>
+:root {
+    @apply bg-slate-900 text-white;
+}
 </style>
