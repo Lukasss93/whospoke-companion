118c9b5b8fde6e4538ea054a83d27114337dd89d
Luca Patera <lucapatera@outlook.it>
2024-09-15 03:21:41 +0200

    Add primevue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 import { useStorage } from '@vueuse/core';
 import {computed} from "vue";
 import { useIpcRendererOn } from '@vueuse/electron';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 
 const domain = useStorage('ws_domain', 'https://whospoke.lucapatera.it');
 const room = useStorage('ws_room', '');
@@ -16,18 +20,25 @@ useIpcRendererOn(window.ipcRenderer,'changeRouteTo', (event, ...args) => {
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center">
+    <div class="min-h-screen flex flex-col items-center justify-center px-2">
         <a href="https://whospoke.lucapatera.it/@Sikuro" target="_blank">
             <img src="/logo.svg" style="width: 80px" class="inline-block" alt="Logo"/>
         </a>
         <p class="text-amber-100 font-bold text-3xl">Who Spoke</p>
-        <p class="text-gray-400 font-bold text-xl">Companion</p>
+        <p class="text-gray-400 font-bold text-xl mb-6">Companion</p>
+
+        <InputGroup class="mb-3">
+            <InputGroupAddon>
+                @
+            </InputGroupAddon>
+            <InputText v-model="room" placeholder="Codice sessione" />
+        </InputGroup>
+
+        <Button label="Entra" severity="info" class="w-full" as="a" link :href="url" />
+
 
 
         <input v-model="domain" placeholder="Dominio" class="m-4"/>
-        <input v-model="room" placeholder="Codice sessione" class="m-4"/>
-
-        <a :href="url" class="m-4 text-white bg-amber-500 p-2 rounded inline-blockl">Accedi</a>
         <p class="text-xs text-white">{{url}}</p>
     </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import './style.scss';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@/presets/Aura';
 
-createApp(App).mount('#app').$nextTick(() => {
-  // Use contextBridge
-  window.ipcRenderer.on('main-process-message', (_event, message) => {
-    console.log(message)
-  })
-})
+createApp(App)
+    .use(PrimeVue, {
+        unstyled: true,
+        pt: Aura,
+    })
+    .mount('#app')
+    .$nextTick(() => {
+        // Use contextBridge
+        window.ipcRenderer.on('main-process-message', (_event, message) => {
+            console.log(message);
+        });
+    });
