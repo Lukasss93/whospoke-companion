9cf9f0f2fbdae1ebda4b485fea624a6b99ced574
Luca Patera <lucapatera@outlook.it>
2024-09-17 08:57:47 +0200

    Fix preset path

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {createApp} from 'vue';
 import './style.scss';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
-import Aura from '@/presets/Aura';
+import Aura from '@/presets/aura';
 
 createApp(App)
     .use(PrimeVue, {
